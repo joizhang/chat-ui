c735639981762df563eb342357827844f32b06ac
joizhang <zxlwhom@outlook.com>
2023-05-04 15:57:56 +0800

    fix: style abnormal of 404 page

--- src/views/exception/404/index.vue
+++ src/views/exception/404/index.vue
@@ -1,19 +1,17 @@
 <template>
-  <v-container class="h-100 d-flex">
-    <v-row class="mt-auto mb-auto">
-      <v-col cols="4" offset="4" class="h-100 text-center">
-        <h1 class="text-center">Sorry, page not found!</h1>
-        <p>
-          Sorry, we couldn't find the page you're looking for. Perhaps you've mistyped the URL? Be sure to check your
-          spelling.
-        </p>
-        <div class="text-center">
-            <img src="../../../icons/404.svg" alt="404" style="width: 500px;" />
-        </div>
+  <div class="d-flex flex-column mb-6 h-100">
+    <div class="ma-auto text-center">
+      <h1>Sorry, page not found!</h1>
+      <p>
+        Sorry, we couldn't find the page you're looking for. Perhaps you've mistyped the URL? Be sure to check your
+        spelling.
+      </p>
+      <img src="../../../icons/404.svg" alt="404" style="width: 500px" />
+      <div>
         <v-btn type="primary" @click="goHome"> Go to Home </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
