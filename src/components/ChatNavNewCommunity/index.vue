99507a0ebb01b1dc2e156c498cc1747d6978edd0
joizhang <zxlwhom@outlook.com>
2023-06-24 19:43:32 +0800

    feat: rename ChatNavNewChatGroup to ChatNavNewCommunity.

---
+++ src/components/ChatNavNewCommunity/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-navigation-drawer class="w-100" :model-value="navModelValue === website.navType.CHAT_NAV_NEW_COMMUNITY">
+    <v-sheet style="background-color: #cc785c">
+      <v-container class="pt-10">
+        <v-row>
+          <v-col cols="auto">
+            <v-btn icon="mdi-keyboard-backspace" color="white" variant="text" @click.stop="handleBack"></v-btn>
+          </v-col>
+          <v-col cols="auto" class="text-center text-white text-h6" style="line-height: 3rem"> Communities </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import website from '@/config/website'
+
+  defineProps({
+    navModelValue: Number,
+  })
+
+  const emit = defineEmits(['changeModelValue'])
+
+  function handleBack() {
+    emit('changeModelValue', website.navType.CHAT_NAV)
+  }
+
+</script>
